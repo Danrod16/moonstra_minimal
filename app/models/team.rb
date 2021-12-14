6922a7628c700b11984a4294ec2e2ee5ae5dc549
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-12-14 17:18:28 +0100

    FIX: Added associations to team && teams_project

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,6 +1,7 @@
 class Team < ApplicationRecord
   validates :name, presence: true, uniqueness: true
-  has_many :projects, through: :teams_projects, dependent: :destroy
+  has_many :teams_project, dependent: :destroy
+  has_many :projects, through: :teams_project, dependent: :destroy
   has_many :users, dependent: :nullify
   def members
     self.users
