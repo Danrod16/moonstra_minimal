e400bb3d93a2cdfe935047fadf14255ff827e42c
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-12-14 17:37:12 +0100

    FEAT: Seeds created

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,5 +1,5 @@
 class Team < ApplicationRecord
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true #uniqueness: true
   has_many :teams_project, dependent: :destroy
   has_many :projects, through: :teams_project, dependent: :destroy
   has_many :users, dependent: :nullify
