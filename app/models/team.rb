aa0b8d6326fbccc194ba69a544268316b6cffb24
Daniel Rodriguez <daniel.rod93@gmail.com>
2021-12-14 17:06:18 +0100

    Feat: added dashboard wrapper and diamonds logic

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,11 +2,19 @@ class Team < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :projects, through: :teams_projects
   has_many :users, dependent: :nullify
+  after_create :welcome_diamonds
   def members
     self.users
   end
 
   def projects
     self.projects
+  end
+
+  private
+
+  def welcome_diamonds
+    self.diamonds =+ 10
+    self.save!
   end
 end
