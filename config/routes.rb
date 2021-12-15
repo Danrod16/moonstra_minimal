8755ced9331d4c19b6a67e229358df1808bf00a0
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-12-15 18:15:16 +0100

    FIX: Deleted show route

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,6 @@ Rails.application.routes.draw do
     get 'overview', to: 'dashboards#overview'
   end
 
-  resources :projects, only: [:new, :create, :index, :show]
+  resources :projects, only: [:new, :create, :index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
