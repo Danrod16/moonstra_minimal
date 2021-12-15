36884d1a16872aeb5c29cc30b817094255e95be7
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-12-15 12:11:30 +0100

    FIX: Added relation btw users & projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,5 +2,5 @@ class Project < ApplicationRecord
   validates :name , presence: true
   validates :description , presence: true
   has_many :teams_projects, dependent: :destroy
-
+  belongs_to :user
 end
