e4cb1d387684fe26b1d58eeeaa28cf2c6b583597
Filip Persson <filip.tillberg.persson@gmail.com>
2021-12-20 16:35:41 +0100

    FEAT: added wizard for join team and create user

--- app/javascript/controllers/wizard_join_team_controller.js
+++ app/javascript/controllers/wizard_join_team_controller.js
@@ -1,4 +1,4 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
   static targets = ['step']
@@ -6,7 +6,7 @@ export default class extends Controller {
   goToNext(event) {
     const nextStep = event.target.dataset.nextStep - 1;
     const actualStep = event.target.dataset.nextStep;
-
+    console.log("sometnig")
     this.stepTargets[nextStep].classList.add("hidden");
     this.stepTargets[actualStep].classList.remove("hidden");
   }
