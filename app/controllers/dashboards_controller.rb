61a1bb07c187745b020434b2cb21c0a0998a697b
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-12-22 14:03:47 +0100

    FIX: refactor method

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,18 +1,21 @@
 class DashboardsController < ApplicationController
   skip_after_action :verify_policy_scoped
   before_action :set_team
+  before_action :find_team_projects
 
   def overview
-    @team_projects = @team.teams_projects
   end
 
   def proposals
-    @teams_projects = @team.teams_projects
   end
 
   private
 
   def set_team
     @team = Team.find(params[:team_id])
+  end
+
+  def find_team_projects
+    @teams_projects = @team.teams_projects
   end
 end
