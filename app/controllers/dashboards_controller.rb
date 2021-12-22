c4e6573bcc26c293a2476ed004c2b96f622c6e76
Alejandro Gorina <alejandrogorinam@gmail.com>
2021-12-22 15:30:16 +0100

    FEAT: Added proposals to yous tab

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,12 +1,13 @@
 class DashboardsController < ApplicationController
   skip_after_action :verify_policy_scoped
-  before_action :set_team
-  before_action :find_team_projects
+  before_action :set_team, :find_team_projects
 
   def overview
   end
 
   def proposals
+    @user_projects = ProjectMember.where(user: current_user)
+
   end
 
   private
